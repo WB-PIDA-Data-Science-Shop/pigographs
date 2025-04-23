5c48e3c625208a9ff61ff049e5133a8f44b6c923
Ileana Marroquin <117444779+ileanamarroquin@users.noreply.github.com>
2025-04-23 11:20:56 -0400

    first commit

---
+++ R/hello.R
@@ -0,0 +1,16 @@
+#' A quick hello function
+#'
+#' @param x a character, what you are interested in printing
+#'
+#'
+#'
+#' @export
+#'
+#' @import stringr stringi
+#'
+
+hello_world <- function(x){
+
+  return(print(x))
+
+}
